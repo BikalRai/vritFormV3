04a6d7b565f990bce45a7c8d42e1e051c5b21a4a
Bikal Rai <bikalrai2785@gmail.com>
2023-01-08 23:41:22 +0545

    added buttons, textfield, appbar, box, icons, etc using mui

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,4 +1,18 @@
+import {
+    Alert,
+    Button,
+    FormControl,
+    IconButton,
+    Input,
+    InputAdornment,
+    InputLabel,
+    TextField,
+} from '@mui/material';
+import { Box } from '@mui/system';
 import React, { useState } from 'react';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import SendIcon from '@mui/icons-material/Send';
 
 const LoginForm = ({
     userDetail: { username: detailUsername, password: detailPassword },
@@ -17,19 +31,20 @@ const LoginForm = ({
     // const { username, password } = user;
 
     const userHandle = ({ target: { value } }) => {
-        setUser({ ...user, username: value });
+        setUser((prev) => {
+            return { ...prev, username: value };
+        });
     };
 
     const passwordHandle = ({ target: { value } }) => {
-        setUser({ ...user, password: value });
+        setUser((prev) => {
+            return { ...prev, password: value };
+        });
     };
 
     const handleLogin = (e) => {
         e.preventDefault();
-        if (
-            user.username === detailUsername &&
-            user.password === detailPassword
-        ) {
+        if (user.username === 'Sam' && user.password === '1234') {
             setIsLoggedIn(true);
             alert(`Welcome ${user.username}`);
         } else {
@@ -37,10 +52,79 @@ const LoginForm = ({
             setIsLoggedIn(false);
         }
     };
+
+    console.log(user);
+
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
     return (
         <>
-            <form>
+            <Box
+                component="form"
+                noValidate
+                autoComplete="off"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                }}
+            >
                 <div className="input-field">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="Username"
+                        variant="standard"
+                        defaultValue="Username"
+                        onChange={userHandle}
+                        value={user.username}
+                    />
+                    {error && user.username === '' && (
+                        <Alert variant="outlined" severity="error">
+                            Do not leave field empty
+                        </Alert>
+                    )}
+                </div>
+                <div className="input-field">
+                    <FormControl
+                        sx={{ m: 1, width: '25ch' }}
+                        variant="standard"
+                    >
+                        <InputLabel htmlFor="standard-adornment-password">
+                            Password
+                        </InputLabel>
+                        <Input
+                            value={user.password}
+                            onChange={passwordHandle}
+                            id="standard-adornment-password"
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {showPassword ? (
+                                            <VisibilityOff />
+                                        ) : (
+                                            <Visibility />
+                                        )}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
+                    {error && user.password === '' && (
+                        <Alert variant="outlined" severity="error">
+                            Do not leave field empty
+                        </Alert>
+                    )}
+                </div>
+                {/* <div className="input-field">
                     <label>Username: </label>
                     <input
                         type="text"
@@ -50,8 +134,8 @@ const LoginForm = ({
                     {error && user.username !== detailUsername && (
                         <p>* No such user</p>
                     )}
-                </div>
-                <div className="input-field">
+                </div> */}
+                {/* <div className="input-field">
                     <label>Password: </label>
                     <input
                         type="password"
@@ -61,8 +145,15 @@ const LoginForm = ({
                 </div>
                 <button type="button" onClick={() => handleLogin}>
                     Login
-                </button>
-            </form>
+                </button> */}
+                <Button
+                    variant="contained"
+                    endIcon={<SendIcon />}
+                    onClick={handleLogin}
+                >
+                    Login
+                </Button>
+            </Box>
         </>
     );
 };
--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -1,4 +1,16 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import React, { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import Alert from '@mui/material/Alert';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import IconButton from '@mui/material/IconButton';
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const RegistrationForm = ({ setIsRegistered, setUserDetail, userDetail }) => {
     const [userInfo, setUserInfo] = useState({
@@ -12,26 +24,35 @@ const RegistrationForm = ({ setIsRegistered, setUserDetail, userDetail }) => {
 
     //user handle
     const handleUser = ({ target: { value } }) => {
-        setUserInfo({ ...userInfo, username: value });
+        setUserInfo((prev) => {
+            return { ...prev, username: value };
+        });
         console.log(username);
     };
     //user password
     const handlePassword = ({ target: { value } }) => {
-        setUserInfo({ ...userInfo, password: value });
+        setUserInfo((prev) => {
+            return { ...prev, password: value };
+        });
         console.log(password);
     };
     //user confirm password
     const handleconfirmPassword = ({ target: { value } }) => {
-        setUserInfo({ ...userInfo, confirmPassword: value });
+        setUserInfo((prev) => {
+            return { ...prev, confirmPassword: value };
+        });
         console.log(confirmPassword);
     };
 
     //handle submit
     const handleSubmit = (e) => {
-        e.preventDefault();
+        // e.preventDefault();
         if (username !== '' && password !== '') {
             if (password === confirmPassword) {
                 alert(`Congratulations on registering ${username}`);
+                // <Alert action severity="success" color="info">
+                //     This is a success alert — check it out!
+                // </Alert>;
                 setEmptyError(false);
                 setPasswordError(false);
                 setIsRegistered(true);
@@ -52,9 +73,161 @@ const RegistrationForm = ({ setIsRegistered, setUserDetail, userDetail }) => {
     console.log(userInfo);
     console.log(userDetail, 'inreg');
 
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
     return (
         <>
-            <form>
+            <Box
+                component="form"
+                noValidate
+                autoComplete="off"
+                sx={{
+                    boxShadow: '2px 3px 10px rgba(0, 0, 0, 0.3)',
+                    padding: '2rem',
+                    width: '60%',
+                    margin: '5rem auto',
+                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                }}
+            >
+                <div className="input-field">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="Username"
+                        variant="standard"
+                        onChange={handleUser}
+                        value={username}
+                    />
+                    {emptyError && username === '' && (
+                        <Alert variant="outlined" severity="error">
+                            Do not leave field empty
+                        </Alert>
+                    )}
+                </div>
+                <div className="input-field">
+                    {/* <TextField
+                        required
+                        id="standard-required"
+                        label="Password"
+                        variant="standard"
+                        onChange={handlePassword}
+                        value={password}
+                    />
+                    {emptyError && password === '' && (
+                        <Alert variant="outlined" severity="error">
+                            Do not leave field empty
+                        </Alert>
+                    )} */}
+                    <FormControl
+                        sx={{ m: 1, width: '25ch' }}
+                        variant="standard"
+                    >
+                        <InputLabel htmlFor="standard-adornment-password">
+                            Password
+                        </InputLabel>
+                        <Input
+                            value={password}
+                            onChange={handlePassword}
+                            id="standard-adornment-password"
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {showPassword ? (
+                                            <VisibilityOff />
+                                        ) : (
+                                            <Visibility />
+                                        )}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
+                    {emptyError && password === '' && (
+                        <Alert variant="outlined" severity="error">
+                            Do not leave field empty
+                        </Alert>
+                    )}
+                </div>
+                <div className="input-field">
+                    {/* <TextField
+                        required
+                        id="standard-required"
+                        label="Confirm Password"
+                        variant="standard"
+                        onChange={handleconfirmPassword}
+                        value={confirmPassword}
+                    />
+                    {emptyError && confirmPassword === '' && (
+                        <Alert variant="outlined" severity="error">
+                            Do not leave field empty
+                        </Alert>
+                    )}
+                    {passwordError && password !== confirmPassword && (
+                        <Alert variant="outlined" severity="error">
+                            Passwords do not match
+                        </Alert>
+                    )} */}
+                    <FormControl
+                        sx={{ m: 1, width: '25ch' }}
+                        variant="standard"
+                    >
+                        <InputLabel htmlFor="standard-adornment-password">
+                            Confirm Password
+                        </InputLabel>
+                        <Input
+                            value={confirmPassword}
+                            onChange={handleconfirmPassword}
+                            id="standard-adornment-password"
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {showPassword ? (
+                                            <VisibilityOff />
+                                        ) : (
+                                            <Visibility />
+                                        )}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
+                    {emptyError && confirmPassword === '' && (
+                        <Alert variant="outlined" severity="error">
+                            Do not leave field empty
+                        </Alert>
+                    )}
+                    {passwordError && password !== confirmPassword && (
+                        <Alert variant="outlined" severity="error">
+                            Passwords do not match
+                        </Alert>
+                    )}
+                </div>
+                <Button
+                    variant="contained"
+                    endIcon={<SendIcon />}
+                    onClick={() => handleSubmit()}
+                >
+                    Register
+                </Button>
+            </Box>
+
+            {/* <form>
                 <div className="input-field">
                     <label>Username: </label>
                     <input type="text" value={username} onChange={handleUser} />
@@ -72,6 +245,9 @@ const RegistrationForm = ({ setIsRegistered, setUserDetail, userDetail }) => {
                     {emptyError && password === '' && (
                         <p>* Field must not be empty</p>
                     )}
+                    {passwordError && password !== confirmPassword && (
+                        <p>* The passwords do not match</p>
+                    )}
                 </div>
                 <div className="input-field">
                     <label>Confirm Password: </label>
@@ -88,7 +264,7 @@ const RegistrationForm = ({ setIsRegistered, setUserDetail, userDetail }) => {
                     )}
                 </div>
                 <button onClick={handleSubmit}>Register</button>
-            </form>
+            </form> */}
         </>
     );
 };
